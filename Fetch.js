392ef91e84a81f69ddac2ea7ebf13bee7966e40a
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2025-01-12 17:21:38 +0200

    package

---
+++ Fetch.js
@@ -0,0 +1,174 @@
+
+/*
+ * Fetch
+ */
+
+class Fetch {
+  
+  defaults = {
+    respType: ['json', 'text'],
+    onlyBody: true,
+    prefix: ''
+  };
+  
+  
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+  methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'];
+  
+  
+  constructor() {
+        
+    this.methods.forEach(method => {
+      
+      method = method.toLowerCase();
+
+      this[method] = this.getMethod(method);
+      
+    });
+    
+  }
+  
+  getMethod(method) {
+    
+    method = method.toUpperCase();
+    
+    return ((resource, options = {}) => {
+      
+      options.method = method;
+      
+      return this.fetch(resource, options);
+      
+    });
+    
+  }
+  
+  
+  async fetch(resource, options = {}) {
+  
+    const { isString, coerceValueToString } = this.util;
+
+
+    // set default options
+    for (const option in this.defaults) {
+
+      if (!(option in options)) {
+
+        options[option] = this.defaults[option];
+
+      }
+
+    }
+    
+    
+    if (isString(options.prefix) &&
+        isString(resource)) {
+  
+      resource = coerceValueToString(resource);
+      const prefix = coerceValueToString(options.prefix);
+      
+      resource = prefix + resource;
+      
+    }
+    
+    
+    const request = fetch(resource, options);
+    
+    let resp = await request;
+    
+    
+    await this.parseRespBody(resp, options.respType);
+
+    if (options.onlyBody) {
+      
+      return resp.body;
+      
+    }
+    
+    return resp;
+    
+  }
+
+
+  async parseRespBody(resp, respType) {
+
+    const { isValidFunction, setReadOnlyProp } = this.util;
+
+
+    if (!Array.isArray(respType)) {
+      
+      if (isValidFunction(respType, resp)) {
+
+        const parse = resp[respType];
+        
+        let body;
+        
+        try {
+          
+          body = await parse.call(resp);
+          
+        } catch(e) {
+          
+          return;
+          
+        }
+        
+        setReadOnlyProp(resp, 'body', body);
+
+        return true;
+        
+      }
+
+    } else if (respType.length !== 0) {
+
+      const didParseBody = await this.parseRespBody(resp, respType[0]);
+
+      if (!didParseBody) {
+      
+        respType.shift();
+
+        return await this.parseRespBody(resp, respType);
+
+      }
+
+      return didParseBody;
+
+    }
+
+  }
+  
+  
+  
+  util = {
+
+    isValidFunction(prop, obj) {
+
+      return (prop in obj &&
+              typeof obj[prop] === 'function');
+      
+    },
+    
+    setReadOnlyProp(obj, prop, value) {
+      
+      Object.defineProperty(obj, prop, {
+        value: value
+      });
+      
+    },
+    
+    isString(x) {
+      return Object.prototype.toString.call(x) === '[object String]'
+    },
+    
+    // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#string_coercion
+    coerceValueToString(value) {
+      
+      return `${value}`;
+      
+    }
+    
+  }
+  
+}
+
+Fetch = new Fetch();
+
---
+++ README.md
@@ -0,0 +1,87 @@
+# Fetch
+
+Upgraded [`fetch`].
+
+## Examples
+
+```JS
+const resp = await Fetch.get('https://api.example.com/movies');
+
+/*
+[{
+  name: 'Example',
+  year: 1975,
+  id: 1
+}]
+*/
+```
+
+```JS
+const resp = await Fetch.get('/movies/1/desc', {
+  prefix: 'https://api.example.com',
+  respType: 'text',
+  onlyBody: false
+});
+
+/*
+Response {
+  body: 'This is an example movie.',
+  ok: true,
+  status: 200,
+  ...
+}
+*/
+```
+
+
+## API
+
+```JS
+const resp = await Fetch.method(resource, ?options);
+```
+
+### method
+
+A lowercased [HTTP request method].
+
+### options
+
+[`fetch` options], plus:
+
+#### respType
+
+[Available types] (plus `stream`).
+
+Attempts to parse `resp` to the provided type(s), then sets `resp.body` to the result.
+
+Can be either a string or an array of multiple strings.  
+If multiple strings are provided, they'll be used as fallbacks for when parsing fails.
+
+Default: `['json', 'text']`.
+
+> Change the default with `Fetch.defaults.respType`.
+
+#### prefix
+
+A prefix to prepend to `resource`.
+
+> Will only be prepended if `resource` and `prefix` are both either strings or [string-coercible].
+
+Default: `''`.
+
+> Change the default with `Fetch.defaults.prefix`.
+
+#### onlyBody
+
+Whether only `resp.body` should be returned, instead of the entire response.
+
+Default: `true`.
+
+> Change the default with `Fetch.defaults.onlyBody`.
+
+
+[`fetch`]: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+[HTTP request method]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+[Available types]: https://developer.mozilla.org/en-US/docs/Web/API/Response#instance_methods
+[`fetch` options]: https://developer.mozilla.org/en-US/docs/Web/API/RequestInit
+[string-coercible]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#string_coercion
